ca5e68db9138f75abf0838aed8c6ade8902ded74
RJ Bergeron <rbergeron@palantir.com>
2018-08-06 13:48:12 -0700

    move docker login to publish job and make conditional

--- publish.sh
+++ publish.sh
@@ -2,6 +2,12 @@
 
 ts=$(date +%s)
 
+docker images "final/*"
+
+[ "${ARTIFACTORY_USERNAME}" ] && {
+  docker login -u "${ARTIFACTORY_USERNAME}" -p "${ARTIFACTORY_PASSWORD}" docker.palantir.build
+}
+
 for img in $(docker images "final/*" --format "{{.Repository}}:{{.Tag}}") ; do
   dest="${img#final/}"
   docker tag "${img}" "${DOCKER_SINK}/${dest}"
