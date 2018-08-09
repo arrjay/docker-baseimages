a9aed9d4a251505450694f6f7f534faaa93f1cb7
RJ Bergeron <rbergero@gmail.com>
2018-08-08 22:14:14 -0700

    publish actual ubuntu versions too

--- publish.sh
+++ publish.sh
@@ -2,8 +2,17 @@
 
 ts=$(date +%s)
 
+set -x
+
 for img in $(docker images "final/*" --format "{{.Repository}}:{{.Tag}}") ; do
   dest="${img#final/}"
+  case "${dest}" in
+    ubuntu:*)
+      vnum=$(docker run --rm=true "${img}" lsb_release -rs)
+      docker tag "${img}" "${DOCKER_SINK}/ubuntu:${vnum}"
+      [ "${NOPUSH}" ] || docker push "${DOCKER_SINK}/ubuntu:${vnum}"
+    ;;
+  esac
   docker tag "${img}" "${DOCKER_SINK}/${dest}"
   docker tag "${img}" "${DOCKER_SINK}/${dest}.${ts}"
   [ "${NOPUSH}" ] || {
