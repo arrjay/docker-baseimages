5c401ab598f791f82452a38dd029c5935709c682
RJ Bergeron <rbergeron@palantir.com>
2018-08-30 13:55:52 -0700

    update publish job to derive minified ubuntu version, not push branches

--- publish.sh
+++ publish.sh
@@ -1,27 +1,37 @@
 #!/usr/bin/env bash
 
-ts=$(date +%s)
+set -eu
 
-docker images "final/*"
-
-[ "${ARTIFACTORY_USERNAME}" ] && {
+[ "${ARTIFACTORY_USERNAME:-}" ] && {
   docker login -u "${ARTIFACTORY_USERNAME}" -p "${ARTIFACTORY_PASSWORD}" docker.palantir.build
 }
 
 set -x
 
+ts=$(date +%s)
+
+docker images "final/*"
+
+[ "${CIRCLE_BRANCH:-}" ] && {
+  case "${CIRCLE_BRANCH}" in
+    master) : ;;
+    *)      NOPUSH=1 ;;
+  esac
+}
+
 for img in $(docker images "final/*" --format "{{.Repository}}:{{.Tag}}") ; do
   dest="${img#final/}"
   case "${dest}" in
     ubuntu:*)
-      vnum=$(docker run --rm=true "${img}" lsb_release -rs)
+      # shellcheck disable=SC2016
+      vnum=$(docker run --rm=true "${img}" bash -c '. /etc/os-release && echo $VERSION_ID')
       docker tag "${img}" "${DOCKER_SINK}/ubuntu:${vnum}"
-      [ "${NOPUSH}" ] || docker push "${DOCKER_SINK}/ubuntu:${vnum}"
+      [ "${NOPUSH:-}" ] || docker push "${DOCKER_SINK}/ubuntu:${vnum}"
     ;;
   esac
   docker tag "${img}" "${DOCKER_SINK}/${dest}"
   docker tag "${img}" "${DOCKER_SINK}/${dest}.${ts}"
-  [ "${NOPUSH}" ] || {
+  [ "${NOPUSH:-}" ] || {
     docker push "${DOCKER_SINK}/${dest}"
     docker push "${DOCKER_SINK}/${dest}.${ts}"
   }
