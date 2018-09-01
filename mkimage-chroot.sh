1896f9853fdf1232b784747e99b6af1609c14cb3
RJ Bergeron <rbergeron@palantir.com>
2018-08-31 17:28:16 -0700

    wrangle mtab after /etc exists

--- mkimage-chroot.sh
+++ mkimage-chroot.sh
@@ -181,9 +181,10 @@ create_chroot_tarball () {
 
   # create config tar
   scratch=$(mktemp -d --tmpdir "$(basename "$0")".XXXXXX)
-  ln -s /proc/mounts   "${scratch}"/etc/mtab
   mkdir -p             "${scratch}"/etc/sysconfig
+  chmod a+rx           "${scratch}"/etc
   chmod a+rx           "${scratch}"/etc/sysconfig
+  ln -s /proc/mounts   "${scratch}"/etc/mtab
   case "${packagemanager}" in
     yum)
   mkdir -p --mode=0755 "${scratch}"/var/cache/yum
