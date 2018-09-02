87f8843d5e27dc69f58fb84fd09e4696be8892b9
RJ Bergeron <rbergero@gmail.com>
2018-09-02 17:30:48 -0400

    silence startup.sh too

--- startup.sh
+++ startup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -ex
+set -e
 
 export DEBIAN_FRONTEND=noninteractive
 
@@ -9,8 +9,11 @@ type dnf && platform="dnf"
 type zypper && platform="zypper"
 [ -f /debootstrap/debootstrap ] && platform="apt"
 
+echo "detected platform ${platform}" 1>&2
+
 case "${platform}" in
   yum|dnf)
+    echo "rebuilding rpm db" 1>&2
     # bring RPM back online from export so it works in chroot.
     dbpath=$(rpm --eval '%_dbpath')
     cd "${dbpath}"
@@ -28,11 +31,13 @@ case "${platform}" in
       rmdir "${rebuilddbdirs[0]}"
     }
 
+    echo "cleaning all yum repos" 1>&2
     "${platform}" clean all
 
     [ -d /etc/yum.repos.d/stage2 ] && {
       stage2_yum=( /etc/yum.repos.d/stage2/*.repo )
       [ -e "${stage2_yum[0]}" ] && {
+        echo "adding additional yum repos" 1>&2
         for r in "${stage2_yum[@]}" ; do
           mv "${r}" /etc/yum.repos.d
         done
@@ -41,23 +46,31 @@ case "${platform}" in
     }
   ;;
   zypper)
+    echo "verifying package installations" 1>&2
     "${platform}" -n verify
   ;;
   apt)
+    echo "running second stage bootstrap" 1>&2
     dpkg-divert --rename /usr/bin/ischroot && ln -s /bin/true /usr/bin/ischroot
     dpkg-divert --rename /usr/sbin/invoke-rc.d && ln -s /bin/true /usr/sbin/invoke-rc.d
     echo "resolvconf resolvconf/linkify-resolvconf boolean false" | debconf-set-selections
     debootstrap_args="--second-stage --merged-usr"
-    read suite < debootstrap/suite
+    read -r suite < debootstrap/suite
     case "${suite}" in
       xenial|precise|trusty) debootstrap_args="--second-stage" ;;
     esac
     /debootstrap/debootstrap ${debootstrap_args} || { cat /debootstrap/debootstrap.log ; exit 1; }
+
+    echo "installing sources.list" 1>&2
     install -m644 /apt-sources.list /etc/apt/sources.list && rm /apt-sources.list
+
+    echo "installing apt-transport-https, debsums, ca-certificates" 1>&2
     apt-get update
     apt-get --no-install-recommends install -qy apt-transport-https debsums ca-certificates
     apt-get -qy dist-upgrade
     debsums_init
+
+    echo "cleaning all apt activity" 1>&2
     apt-get clean all
     rm -rf /var/lib/apt/lists/*
     rm /usr/bin/ischroot && dpkg-divert --rename --remove /usr/bin/ischroot
@@ -65,10 +78,10 @@ case "${platform}" in
   ;;
 esac
 
-[ ! -d /etc/stamps.d ] && rm -rf /etc/stamps.d && mkdir /etc/stamps.d
-date "+%s" > /etc/stamps.d/base.stamp
-
 # if we find ourselves, delete ourselves.
-if [[ -s "$BASH_SOURCE" ]] && [[ -x "$BASH_SOURCE" ]]; then
-        rm "$(readlink -f "$BASH_SOURCE")"
-fi
+# shellcheck disable=SC2128
+{
+  if [[ -s "$BASH_SOURCE" ]] && [[ -x "$BASH_SOURCE" ]]; then
+          rm "$(readlink -f "$BASH_SOURCE")"
+  fi
+}
