6f745351e259182445d4e096a741fe52048e8f33
RJ Bergeron <rbergero@gmail.com>
2018-09-02 17:16:56 -0400

    revise mkimage-chroot to be quieter

--- mkimage-chroot.sh
+++ mkimage-chroot.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -eux
+set -eu
 set -o pipefail
 
 # locate source files
@@ -17,6 +17,7 @@ devtgz="${srcdir}/devs.tar.gz"
 debootstrap_dir="${srcdir}/debootstrap"
 
 [ "${UBUNTU_URI:=}" ] || UBUNTU_URI=https://mirrors.kernel.org/ubuntu/
+echo "using ${UBUNTU_URI} as debootstrap mirror."
 
 # reset umask
 umask 0022
@@ -27,22 +28,46 @@ if [ ! -f "${devtgz}" ] ; then
   exit 2
 fi
 
+# get checkout directory git revision information
+coderev="$(git rev-parse --short HEAD)"
+  # uncommitted
+{ git diff-index --quiet --cached HEAD -- &&
+  # extra
+  git diff-files --quiet ; } || coderev="${coderev}-DIRTY"
+
+case "${coderev}" in
+  *-DIRTY) echo "WARNING: git tree is dirty, sleeping 5 seconds for running confirmation. build will not use cache."
+           sleep 5
+           FORCE_BUILD=1
+           ;;
+esac
+
+timestamp="$(date +%s)"
+
+echo "building ${coderev} at ${timestamp}"
+
 # create a scratch directory to use for working files
 wkdir=$(env TMPDIR=/var/tmp mktemp -d)
 export TMPDIR="${wkdir}"
 
 __cleanup () {
-  [ -z "${NOCLEAN:-}" ] && sudo rm -rf "${wkdir}"
+  echo "working directory was ${wkdir}" 1>&2
+  [ -z "${NOCLEAN:-}" ] && { echo "removing workdir" 1>&2 ; sudo rm -rf "${wkdir}" ; }
 }
 
 trap __cleanup EXIT ERR
 
 sudo () { env "$@"; }
 # if we're not root, bring sudo to $sudo
-[ "$(id -u)" != "0" ] && sudo () { command sudo env "$@"; }
+{
+  [ "$(id -u)" != "0" ] && {
+    echo "you are not root, wrapping commands in sudo..." 1>&2
+    sudo () { command sudo env "$@"; }
+  } ;
+} || echo "your are already root, 'sudo' is env in this script."
 
 create_chroot_tarball () {
-  local packagemanager distribution release subdir debootstrap
+  local packagemanager distribution release subdir __debootstrap debootstrap_file __centos_contentdir
   local repos_d repos2_d gpg
   subdir="${1}"
   packagemanager="${subdir%/*}"
@@ -55,7 +80,6 @@ create_chroot_tarball () {
   gpg_keydir="${subdir}/gpg-keys"
   # shellcheck disable=SC2015
   [ ! -d "${gpg_keydir}" ] && { echo "missing ${gpg_keydir}" 1>&2 ; exit 1 ; } || true
-  local deboostrap_file
   debootstrap_file="${debootstrap_dir}/scripts/${release}"
 
   # if we didn't get packagemanager, distribution display usage
@@ -68,28 +92,95 @@ create_chroot_tarball () {
     *) echo "unknown packagemanager" 1>&2 ; exit 240 ;;
   esac
 
+  # reread arch at this point, in case we are building for...not x86_64/amd64.
+  [ -f "${subdir}/arch" ] && read -r arch < "${subdir}/arch"
+
   # mock out commands via function overload here - which is exactly what we want, but drives shellcheck batty.
   # shellcheck disable=SC2032,SC2033
-  rpm() { sudo rpm --root "${rootdir}" "${@}"; }
-  [ -f "${subdir}/arch" ] && read -r arch < "${subdir}/arch"
-  debootstrap() { sudo DEBOOTSTRAP_DIR="$(pwd)/debootstrap" bash -x "${debootstrap}" --verbose --variant=minbase "--arch=${arch}" "${@}" "${rootdir}" "${UBUNTU_URI}" ; }
-  yum() { sudo yum --releasever="${release}" --installroot="${rootdir}" -c "${yumconf}" "${@}"; }
-  # if we're actually using dnf, do not install weak dependencies
-  type dnf 2> /dev/null 1>&2 && yum() { sudo dnf --setopt=install_weak_deps=False --best --releasever="${release}" --installroot="${rootdir}" -c "${yumconf}" "${@}" ; }
+  {
+    type rpm 2> /dev/null 1>&2 && {
+      # any RPM calls here should be in our chroot.
+      rpm() { sudo rpm --root "${rootdir}" "${@}"; }
+    }
+    type debootstrap 2> /dev/null 1>&2 && {
+      __debootstrap="$(which debootstrap)"
+      echo "deboostrap is ${__debootstrap}" 1>&2
+      debootstrap() {
+        # run using our included function set
+        # with a minimal install
+        # for our architecture
+        # with flags as called
+        # in our chroot
+        # using the UBUNTU_URI mirror.
+        sudo DEBOOTSTRAP_DIR="${srcdir}/debootstrap" \
+          bash "${__debootstrap}" \
+            --verbose \
+            --variant=minbase \
+            "--arch=${arch}" \
+            "${@}" \
+            "${rootdir}" \
+            "${UBUNTU_URI}"
+      }
+    }
+    { type dnf 2> /dev/null 1>&2 && {
+      echo "using dnf for yum calls" 1>&2
+      yum() {
+        # call with flags to disable weak dependencies
+        # manually set release version
+        # into the chroot
+        # using a config we made
+        # with flags as called
+        sudo dnf \
+          --setopt=install_weak_deps=False --best \
+          --releasever="${release}" \
+          --installroot="${rootdir}" \
+          -c "${yumconf}" \
+          "${@}"
+        }
+      }
+      true
+    } ||
+    {
+      # should only run if we did _not_ just set up dnf
+      type yum 2>/dev/null 1>&2 && {
+      echo "using yum for yum calls" 1>&2
+      yum() {
+        # manually set release version
+        # into the chroot
+        # using a config we made
+        # with flags as called
+        sudo yum \
+          --releasever="${release}" \
+          --installroot="${rootdir}" \
+          -c "${yumconf}" \
+          "${@}"
+        }
+      }
+    }
+  }
 
   # let's go!
   rootdir=$(mktemp -d)
   conftar=$(mktemp --tmpdir conf.XXX.tar)
 
+  echo "building chroot tarball expecting system package manager ${packagemanager}" 1>&2
+  echo "target system is ${distribution}-${release}-${arch}" 1>&2
+
   case "${packagemanager}" in
     yum|dnf|zyp)
       # init rpm, add gpg keys and release rpm
+      echo "initializing RPM database with system RPM" 1>&2
       rpm --initdb
+      echo "importing GPG keys" 1>&2
       for gpg in "${gpg_keydir}"/* ; do
         rpm --import "${gpg}"
       done
+      echo "installing base packages from configdir" 1>&2
       rpm -iv --nodeps "${subdir}/*.rpm"
+
       centos_ver=$(rpm -q --qf "%{VERSION}" centos-release || true)
+
+      echo "installing rpm repository definitions" 1>&2
       repos_d=( "${subdir}/yum.repos.d"/*.repo )
       if [ -e "${repos_d[0]}" ] ; then
         for f in "${repos_d[@]}" ; do
@@ -118,12 +209,16 @@ create_chroot_tarball () {
           sudo install -D -m644 "${add_stage2_yumconf[@]}" "${rootdir}/etc/yum.repos.d/stage2/"
         }
       }
+
+      echo "configuring system package manager chain to install in chroot" 1>&2
       case "${distribution}" in
         centos*)
           inst_packages=(@Base yum yum-plugin-ovl yum-utils centos-release)
           # https://lists.centos.org/pipermail/centos-devel/2018-March/016542.html
           sudo mkdir -p "${rootdir}/etc/yum/vars"
-          [ "${arch}" == 'x86_64' ]  && echo 'centos' | sudo tee "${rootdir}/etc/yum/vars/contentdir" || echo 'altarch' | sudo tee "${rootdir}/etc/yum/vars/contentdir"
+          __centos_contentdir='centos'
+          [ "${arch}" != 'x86_64' ] && __centos_contentdir='altarch'
+          echo "${__centos_contentdir}"  sudo tee "${rootdir}/etc/yum/vars/contentdir" > /dev/null
         ;;
         fedora*)
           inst_packages=("@Minimal Install" dnf fedora-release fedora-release-notes fedora-gpg-keys)
@@ -143,8 +238,12 @@ create_chroot_tarball () {
       yumconf=$(mktemp --tmpdir yum.XXXX.conf)
       sudo cp "${rootdir}/etc/yum.conf" "${yumconf}"
       printf 'reposdir=%s\n' "${rootdir}/etc/yum.repos.d" >> "${yumconf}"
+
+      echo "installing base package dependencies in chroot using host manager" 1>&2
       yum repolist -v
       yum install -y "${inst_packages[@]}"
+
+      echo "configuring ostree-compatible rpm db path for chroot" 1>&2
       # wire the rpmdb move here...
       [ "${packagemanager}" != "zyp" ] && {
         # see http://lists.rpm.org/pipermail/rpm-maint/2017-October/006681.html - moving the rpm dbs out of /var/lib/rpm
@@ -154,32 +253,44 @@ create_chroot_tarball () {
       }
     ;;
     apt)
-      debootstrap=$(which debootstrap)
       keyring=( "${subdir}/gpg-keys"/*.gpg )
       debootstrap_args="--foreign --merged-usr ${release}"
       case "${release}" in
         precise|trusty|xenial) debootstrap_args="--foreign ${release}"
       esac
+
+      echo "installing system using host debootstrap" 1>&2
       debootstrap --keyring="${keyring[0]}" ${debootstrap_args} || true
+
+      echo "configuring essential packages for in-docker operation" 1>&2
       sudo mkdir -p --mode=0755 "${rootdir}/var/lib/resolvconf" && sudo touch "${rootdir}/var/lib/resolvconf/linkified"
+
+      echo "installing sources.list files from config directories" 1>&2
       [ -f "${subdir}/sources.list" ] && sudo install -m644 "${subdir}/sources.list" "${rootdir}/apt-sources.list"
       [ "${ADDITIONAL_CONFIG_DIR:-}" ] && {
         [ -f "${ADDITIONAL_CONFIG_DIR}/${subdir#config/}/sources.list" ] && \
           sudo install -m644 "${ADDITIONAL_CONFIG_DIR}/${subdir#config/}/sources.list" "${rootdir}/apt-sources.list"
       }
+
+      echo "installing GPG keyrings" 1>&2
       case "${distribution}" in
         ubuntu*) sudo mkdir -p --mode=0755 "${rootdir}/usr/share/keyrings" && sudo install -m644 "${keyring[0]}" "${rootdir}/usr/share/keyrings/ubuntu-archive-keyring.gpg" ;;
       esac
     ;;
   esac
 
-  [ -f "${rootdir}/etc/machine-id" ] && : | sudo tee "${rootdir}/etc/machine-id"
+  [ -f "${rootdir}/etc/machine-id" ] && {
+    echo "erasing /etc/machine-id in chroot" 1>&2
+    : | sudo tee "${rootdir}/etc/machine-id"
+  }
 
+  echo "creating raw distribution tarball ${distribution}-${release}-${arch}.tar"
   # I need sudo for _read_ permissions, but you can own this fine.
   # shellcheck disable=SC2024
-  sudo tar cp '--exclude=./dev*' -C "${rootdir}" . > "${distribution}-${release}.tar"
+  sudo tar cp '--exclude=./dev*' -C "${rootdir}" . > "${distribution}-${release}-${arch}.tar"
 
   # create config tar
+  echo "creating in-docker configuration files" 1>&2
   scratch=$(mktemp -d --tmpdir "$(basename "$0")".XXXXXX)
   mkdir -p             "${scratch}"/etc/sysconfig
   chmod a+rx           "${scratch}"/etc
@@ -187,14 +298,14 @@ create_chroot_tarball () {
   ln -s /proc/mounts   "${scratch}"/etc/mtab
   case "${packagemanager}" in
     yum)
-  mkdir -p --mode=0755 "${scratch}"/var/cache/yum
+      mkdir -p --mode=0755 "${scratch}"/var/cache/yum
+      printf 'NETWORKING=yes\nHOSTNAME=localhost.localdomain\n' > "${scratch}"/etc/sysconfig/network
     ;;
   esac
   cp       startup.sh  "${scratch}"/startup
   mkdir -p --mode=0755 "${scratch}"/var/cache/ldconfig
-  printf 'NETWORKING=yes\nHOSTNAME=localhost.localdomain\n' > "${scratch}"/etc/sysconfig/network
   printf '127.0.0.1   localhost localhost.localdomain\n'    > "${scratch}"/etc/hosts
-  tar --numeric-owner --group=0 --owner=0 -c -C "${scratch}" --files-from=- -f "${conftar}" << EOA || true
+  tar --numeric-owner --group=0 --owner=0 -c -C "${scratch}" --files-from=- -f "${conftar}" 2>/dev/null << EOA || true
 ./etc/mtab
 ./etc/hosts
 ./etc/sysconfig/network
@@ -210,7 +321,8 @@ EOA
   rpmdbfiles=$(mktemp --tmpdir "$(basename "$0")".XXXXXX)
 
   # ubuntu/debian do stupid things to rpm.
-  os_like=$(. /etc/os-release ; echo ${ID_LIKE:-})
+  # shellcheck disable=SC1091
+  os_like=$(. /etc/os-release ; echo "${ID_LIKE:-}")
   case "${os_like}" in
     debian) rpmdbdir="${HOME}/.rpmdb" ;;
     *)      rpmdbdir="/var/lib/rpm" ;;
@@ -218,36 +330,40 @@ EOA
 
   case "${packagemanager}" in
     yum|dnf)
+      echo "performing rpm db export from host rpm databases" 1>&2
       # use this for rpmdb extraction
-      tar --list --file="${distribution}-${release}.tar" | grep "^.${rpmdbdir}" | grep -v '/$' | tee "${rpmdbfiles}"
+      tar --list --file="${distribution}-${release}-${arch}.tar" | \
+       grep "^.${rpmdbdir}" | grep -v '/$' | tee "${rpmdbfiles}" > /dev/null
 
-    rpmdb_dump="/usr/lib/rpm/rpmdb_dump"
-    [ -e "${rpmdb_dump}" ] || {
-      libdb="$(ldd "$(which rpm)"|grep libdb|cut -d= -f1)"
-      libdb="${libdb#"${libdb%%[![:space:]]*}"}"
-      libdb="${libdb%"${libdb##*[![:space:]]}"}"
-      libdb="${libdb#*-}"
-      libdb="${libdb%.so}"
-      rpmdb_dump="db${libdb}_dump"
-    }
-    rpmdb_extract_dir=$(mktemp -d --tmpdir "$(basename "$0")".XXXXXX)
-    rpmdb_dumpfiles=$(mktemp --tmpdir "$(basename "$0")".rpmdbdump.XXXXXX)
-    # first, pry the rpmdb out.
-    tar -C "${rpmdb_extract_dir}" --extract --file="${distribution}-${release}".tar --files-from="${rpmdbfiles}"
-    mkdir -p "${rpmdb_extract_dir}${target_rpmdbdir}"
-    # convert db files to dump files
-    for x in "${rpmdb_extract_dir}${rpmdbdir}"/* ; do
-      dumpfile="$(basename "${x}").dump"
-      "${rpmdb_dump}" "${x}" > "${rpmdb_extract_dir}${target_rpmdbdir}/${dumpfile}"
-      echo ".${target_rpmdbdir}/${dumpfile}" >> "${rpmdb_dumpfiles}"
-      rm "${x}"
-    done
+      rpmdb_dump="/usr/lib/rpm/rpmdb_dump"
+      [ -e "${rpmdb_dump}" ] || {
+        libdb="$(ldd "$(which rpm)"|grep libdb|cut -d= -f1)"
+        libdb="${libdb#"${libdb%%[![:space:]]*}"}"
+        libdb="${libdb%"${libdb##*[![:space:]]}"}"
+        libdb="${libdb#*-}"
+        libdb="${libdb%.so}"
+        rpmdb_dump="db${libdb}_dump"
+      }
+      rpmdb_extract_dir=$(mktemp -d --tmpdir "$(basename "$0")".XXXXXX)
+      rpmdb_dumpfiles=$(mktemp --tmpdir "$(basename "$0")".rpmdbdump.XXXXXX)
+      # first, pry the rpmdb out.
+      tar -C "${rpmdb_extract_dir}" --extract --file="${distribution}-${release}-${arch}".tar --files-from="${rpmdbfiles}"
+      mkdir -p "${rpmdb_extract_dir}${target_rpmdbdir}"
+      # convert db files to dump files
+      for x in "${rpmdb_extract_dir}${rpmdbdir}"/* ; do
+        dumpfile="$(basename "${x}").dump"
+        "${rpmdb_dump}" "${x}" > "${rpmdb_extract_dir}${target_rpmdbdir}/${dumpfile}"
+        echo ".${target_rpmdbdir}/${dumpfile}" >> "${rpmdb_dumpfiles}"
+        rm "${x}"
+      done
 
-    tar --numeric-owner --group=0 --owner=0 -C "${rpmdb_extract_dir}" --create --file="${distribution}-${release}"-rpmdb.tar --files-from=- < "${rpmdb_dumpfiles}"
+      tar --numeric-owner --group=0 --owner=0 -C "${rpmdb_extract_dir}" --create \
+       --file="${distribution}-${release}-${arch}"-rpmdb.tar --files-from=- < "${rpmdb_dumpfiles}"
     ;;
   esac
 
-  tar --delete --file="${distribution}-${release}".tar --files-from=- << EOA || true
+  echo "deleting unwanted or overriden files from chroot tarball" 1>&2
+  tar --delete --file="${distribution}-${release}-${arch}".tar --files-from=- << EOA 2>/dev/null || true
 ./etc/mtab
 ./usr/lib/locale
 ./usr/share/locale
@@ -276,10 +392,16 @@ $(cat "${rpmdbfiles}")
 EOA
 
   # bring it all together
-  tar --concatenate --file="${distribution}-${release}".tar "${devtar}"
-  tar --concatenate --file="${distribution}-${release}".tar "${conftar}"
+  echo "adding device and configuration tarballs to chroot tarball" 1>&2
+  tar --concatenate --file="${distribution}-${release}-${arch}".tar "${devtar}"
+  tar --concatenate --file="${distribution}-${release}-${arch}".tar "${conftar}"
   case "${packagemanager}" in
-    yum|dnf) tar --concatenate --file="${distribution}-${release}.tar" "${distribution}-${release}-rpmdb.tar" && rm "${distribution}-${release}-rpmdb.tar" ;;
+    yum|dnf)
+      echo "adding rpm database export to chroot tarball" 1>&2
+      tar --concatenate --file="${distribution}-${release}-${arch}.tar" \
+        "${distribution}-${release}-${arch}-rpmdb.tar"
+      rm "${distribution}-${release}-${arch}-rpmdb.tar"
+    ;;
   esac
 }
 
@@ -291,13 +413,20 @@ docker_init () {
   distribution="${subdir#*${packagemanager}/}"
   release="${distribution#*-}"
   distribution="${distribution%-${release}}"
-  docker import "${distribution}-${release}.tar" "pre/${distribution}-${release}"
-  rm "${distribution}-${release}.tar"
-  docker run -i --name "setup_${distribution}-${release}" -t "pre/${distribution}-${release}" /startup
-  docker export "setup_${distribution}-${release}" | docker import - "build/${distribution}-${release}"
-  docker rm "setup_${distribution}-${release}"
-  docker rmi "pre/${distribution}-${release}"
+  echo "importing chroot tarball" 1>&2
+  docker import "${distribution}-${release}-${arch}.tar" "pre/${distribution}-${release}-${arch}"
+  rm "${distribution}-${release}-${arch}.tar"
 
+  echo "running initial docker setup" 1>&2
+  docker run -i --name "setup_${distribution}-${release}-${arch}" -t "pre/${distribution}-${release}-${arch}" /startup
+
+  # FIXME dropping the architecture right here for now
+  echo "importing docker-ready image" 1>&2
+  docker export "setup_${distribution}-${release}-${arch}" | docker import - "build/${distribution}-${release}"
+  docker rm "setup_${distribution}-${release}-${arch}"
+  docker rmi "pre/${distribution}-${release}-${arch}"
+
+  echo "checking for immediate package updates in image - verifying package manager works" 1>&2
   docker_check "build/${distribution}-${release}" "${packagemanager}" && {
     docker tag "build/${distribution}-${release}" "stage2/${distribution}-${release}"
     docker rmi "build/${distribution}-${release}"
@@ -309,6 +438,7 @@ docker_check () {
   image="${1}"
   packagemanager="${2}"
 
+  echo "checking for package updates in ${image} using ${packagemanager}" 1>&2
   case "${packagemanager}" in
     yum|dnf) docker run --rm=true "${image}" "${packagemanager}" check-update ;;
     zyp) docker run --rm=true "${image}" zypper patch-check ;;
@@ -318,7 +448,6 @@ docker_check () {
 }
 
 check_existing () {
-  [ "${FORCE_BUILD:=}" ] && return 1
   local packagemanager distribution release subdir
   subdir="${1}"
   packagemanager="${subdir%/*}"
@@ -327,12 +456,13 @@ check_existing () {
   release="${distribution#*-}"
   distribution="${distribution%-${release}}"
 
-  if [ "${DOCKER_SINK:=''}" ] ; then
+  if [ -z "${DOCKER_SINK:-}" ] ; then
+    echo "DOCKER_SINK unset, cannot use cache - no publishing reference!" 1>&2
+    return 1
+  else
+    [ "${FORCE_BUILD:-}" ] && return 1
     docker_check "${DOCKER_SINK}/${distribution}:${release}" "${packagemanager}" && \
       docker tag "${DOCKER_SINK}/${distribution}:${release}" "final/${distribution}:${release}"
-  else
-    docker rmi -f "${DOCKER_SINK}/${distribution}:${release}"
-    return 1
   fi
 }
 
