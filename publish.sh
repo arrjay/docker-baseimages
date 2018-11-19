5de0cf9ea28b3c3bcd4fcea4e695f687bc2650b0
RJ Bergeron <rbergero@gmail.com>
2018-11-19 00:10:04 -0800

    update publish script for buildah

--- publish.sh
+++ publish.sh
@@ -1,26 +1,25 @@
 #!/usr/bin/env bash
 
-set -x
+set +x
 
-for img in $(docker images "final/*" --format "{{.Repository}}:{{.Tag}}") ; do
-  dest="${img#final/}"
-  ts=$(docker run --rm=true "${img}" cat /etc/stamps.d/base-build.stamp)
-  cr=$(docker run --rm=true "${img}" cat /etc/stamps.d/base-code.stamp)
+for img in $(sudo buildah images --json | jq -ars 'unique_by(.id)|.[].names[]|select(contains("localhost/final"))') ; do
+  dest="${img#localhost/final/}"
+  ts=$(sudo podman run --rm=true "${img}" cat /etc/stamps.d/base-build.stamp)
+  cr=$(sudo podman run --rm=true "${img}" cat /etc/stamps.d/base-code.stamp)
   case "${dest}" in
     ubuntu:*)
-      vnum=$(docker run --rm=true "${img}" bash -c '. /etc/os-release && echo $VERSION_ID')
-      docker tag "${img}" "${DOCKER_SINK}/ubuntu:${vnum}"
-      [ "${NOPUSH}" ] || docker push "${DOCKER_SINK}/ubuntu:${vnum}"
+      vnum=$(sudo podman run --rm=true "${img}" bash -c '. /etc/os-release && echo $VERSION_ID')
+      buildah tag "${img}" "${DOCKER_SINK}/ubuntu:${vnum}"
+      [ "${NOPUSH}" ] || sudo buildah push --creds "${DOCKER_USER}:${DOCKER_PASS}" "${DOCKER_SINK}/ubuntu:${vnum}" "docker://${DOCKER_SINK}/ubuntu:${vnum}"
     ;;
   esac
-  docker tag "${img}" "${DOCKER_SINK}/${dest}"
-  docker tag "${img}" "${DOCKER_SINK}/${dest}.${ts}"
-  docker tag "${img}" "${DOCKER_SINK}/${dest}.${ts}.${cr}"
+  sudo buildah tag "${img}" "${DOCKER_SINK}/${dest}"
+  sudo buildah tag "${img}" "${DOCKER_SINK}/${dest}.${ts}"
+  sudo buildah tag "${img}" "${DOCKER_SINK}/${dest}.${ts}.${cr}"
   [ "${NOPUSH}" ] || {
-    docker push "${DOCKER_SINK}/${dest}"
-    docker push "${DOCKER_SINK}/${dest}.${ts}"
-    docker push "${DOCKER_SINK}/${dest}.${ts}.${cr}"
+    sudo buildah push --creds "${DOCKER_USER}:${DOCKER_PASS}" "${DOCKER_SINK}/${dest}"             "docker://${DOCKER_SINK}/${dest}"
+    sudo buildah push --creds "${DOCKER_USER}:${DOCKER_PASS}" "${DOCKER_SINK}/${dest}.${ts}"       "docker://${DOCKER_SINK}/${dest}.${ts}"
+    sudo buildah push --creds "${DOCKER_USER}:${DOCKER_PASS}" "${DOCKER_SINK}/${dest}.${ts}.${cr}" "docker://${DOCKER_SINK}/${dest}.${ts}.${cr}"
   }
-  docker rmi "${img}"
 done
 
