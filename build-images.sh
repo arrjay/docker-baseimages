a02e234af2da54bed25915b0bf88a4a9dbe2a8ba
RJ Bergeron <git@untrusted.host>
2018-12-26 18:39:46 -0500

    dind is the CI way to go - make wrappers?

--- build-images.sh
+++ build-images.sh
@@ -5,6 +5,26 @@ set -x
 # first...who am I? what is this?
 id
 
-podman images
-buildah images
-buildah containers
+# stub in/out docker or libpod equivalents
+[ "${USE_BUILDAH:-}" == true ] && {
+  _buildah_status () {
+    buildah images
+    buildah containers
+  }
+
+  _podman_images () {
+    podman images
+  }
+} || {
+  _buildah_status () {
+    docker images
+    docker ps -a
+  }
+
+  _podman_images () {
+    docker images
+  }
+}
+
+_podman_images
+_buildah_status
--- prereqs.sh
+++ prereqs.sh
@@ -2,7 +2,12 @@
 
 # install any missing prereqs - assumes ubuntu 18.04 from upstream
 
-[ -f /etc/apt/sources.list.d/projectatomic-ubuntu-ppa-bionic.list ] || add-apt-repository -y ppa:projectatomic/ppa
+[ "${USE_BUILDAH:-}" == "true" ] && {
+  [ -f /etc/apt/sources.list.d/projectatomic-ubuntu-ppa-bionic.list ] || add-apt-repository -y ppa:projectatomic/ppa
 
-type buildah || apt-get install -q -y buildah
-type podman  || apt-get install -q -y podman
+  type buildah || apt-get install -q -y buildah
+  type podman  || apt-get install -q -y podman
+} || {
+  # use docker.io as shipped with Ubuntu kplzthx
+  type docker || apt-get install -q -y docker.io
+}
