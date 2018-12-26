4524a111f8794ab0cb64cd5ab952d4f4ae9d3bac
RJ Bergeron <git@untrusted.host>
2018-12-26 18:48:15 -0500

    apt-get updates, install git for cicheck

--- prereqs.sh
+++ prereqs.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # install any missing prereqs - assumes ubuntu 18.04 from upstream
+apt-get update
 
 [ "${USE_BUILDAH:-}" == "true" ] && {
   [ -f /etc/apt/sources.list.d/projectatomic-ubuntu-ppa-bionic.list ] || add-apt-repository -y ppa:projectatomic/ppa
